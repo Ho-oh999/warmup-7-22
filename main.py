65b839e01901bcf97d7d7ee4d1ec19fba7d601f6
boko999 <ho-oh999@hotmail.com>
2020-07-22 17:34:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# for i in range (4):
+  #print ("go cats")
+
+#for i in range(4):
+  #print("bye")
+
+#for i in range(4):
+  #for i in range(5):
+    #  print("hi")
+
+
+
+
+num = int(input ('give me a number positive or negative:'))
+if num >= 1:
+  print("greater than 0")
+elif num <= -1:
+  print("less then 0")
+else:
+  print ("equal to 0")
+print ("your number is: ")
+print (num)
+
+
